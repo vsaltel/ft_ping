b0822333469a066dc9ef3c73a578632773da0226
Valentin <saltel.valentin@gmail.com>
2021-03-11 15:11:48 +0100

    time

--- srcs/ping.c
+++ srcs/ping.c
@@ -92,7 +92,7 @@ static int	send_loop(t_ping *ping, int sock)
   			{ 
 		*/
 				gettimeofday(&aft, NULL);
-  				ft_printf("%zd bytes from %s (%s): icmp_seq=%d ttl=%d time=%.2f ms\n",  
+  				printf("%zd bytes from %s (%s): icmp_seq=%d ttl=%d time=%.2f ms\n",  
   					recv_bytes, ping->dest_name, ping->dest_ip, ping->msg_count, PING_TTL, (float)(aft.tv_usec - bef.tv_usec) / 1000); 
   				ping->msg_recv_count++; 
 				ping->total_stime = (ping->total_stime == -1 ? 0 : ping->total_stime + (aft.tv_usec - bef.tv_usec));

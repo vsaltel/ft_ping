8b892c258dd8f350d9d512ac664be48dfe182044
Valentin <saltel.valentin@gmail.com>
2021-03-11 16:14:35 +0100

    time

--- srcs/main.c
+++ srcs/main.c
@@ -7,24 +7,22 @@ int		main(int argc, char **argv)
 	t_ping	sping;
 
 	g_state = 1;
+	if (getuid() != 0)
+	{
+		ft_printf("ft_ping: need root privilege\n");
+		return (1);
+	}
 	if (check_args(argc, argv))
 	{
 		print_usage();
-		return (1);
+		return (2);
 	}
+	init_ping(&sping);
 	get_args(&sping, argc, argv);
 	if (sping.h)
 		print_usage();
 	else
-	{
-		if (getuid() != 0)
-		{
-			ft_printf("ft_ping: need root privilege\n");
-			return (2);
-		}
-		init_ping(&sping);
 		ping(&sping);
-	}
 	free_args(&sping);
 	return (0);
 }

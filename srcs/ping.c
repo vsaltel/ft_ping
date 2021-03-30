c9d4b972d2519724a94403afaf519358abad1001
Valentin <saltel.valentin@gmail.com>
2021-03-30 14:08:48 +0200

    test

--- srcs/ping.c
+++ srcs/ping.c
@@ -126,9 +126,10 @@ int			ping(t_ping *ping)
 	int				sock;
 
 	ft_memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET6; // IPv4 ou IPv6
+	hints.ai_family = AF_UNSPEC; // IPv4 ou IPv6
+	//hints.ai_family = AF_INET6; // IPv4 ou IPv6
 	hints.ai_socktype = SOCK_STREAM; // Une seule famille de socket
-	hints.ai_flags = AI_CANONNAME;
+	//hints.ai_flags = AI_CANONNAME;
 	if (getaddrinfo(ping->dest_name, NULL, &hints, &res) != 0)
 	{
 		ft_dprintf(2, "ft_ping: %s: No address associated with hostname\n", ping->dest_name);

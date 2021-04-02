22bc3e7157706995bdb7ae83862eba50fb3b2dcf
Valentin <saltel.valentin@gmail.com>
2021-04-02 02:40:57 +0200

    test1

--- srcs/rev_dns.c
+++ srcs/rev_dns.c
@@ -8,8 +8,8 @@ struct addrinfo	*reverse_dns_info(char *host, char *serv, int family, int sockty
 	ft_memset(&hints, 0, sizeof hints);
 	hints.ai_family = family;
 	hints.ai_socktype = socktype;
-	hints.ai_flag = AI_CANONNAME;
-	if (getaddrinfo(ping->dest_name, serv, &hints, &res) != 0)
-		ft_dprintf(2, "ft_ping: %s: No address associated with hostname\n", ping->dest_name);
+	hints.ai_flags = AI_CANONNAME;
+	if (getaddrinfo(host, serv, &hints, &res) != 0)
+		ft_dprintf(2, "ft_ping: %s: No address associated with hostname\n", host);
 	return (res);
 }

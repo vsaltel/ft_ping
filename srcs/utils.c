14a1588ad154a5c84f617044e0dc99d3e81a2871
Valentin <saltel.valentin@gmail.com>
2021-04-06 20:57:11 +0200

    option v

--- srcs/utils.c
+++ srcs/utils.c
@@ -2,10 +2,12 @@
 
 unsigned short	checksum(void *b, int len) 
 {
-	unsigned short *buf = b; 
-	unsigned int sum=0; 
-	unsigned short result; 
+	unsigned short	*buf; 
+	unsigned int	sum; 
+	unsigned short	result; 
 	
+	buf = b;
+	sum = 0;
 	for ( sum = 0; len > 1; len -= 2 ) 
 		sum += *buf++; 
 	if ( len == 1 ) 

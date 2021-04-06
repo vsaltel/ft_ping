61b8be155a7405a6c25eb430467bc3739b8a4a43
Valentin <saltel.valentin@gmail.com>
2021-04-06 13:57:51 +0200

    test1

--- srcs/recv_msg.c
+++ srcs/recv_msg.c
@@ -14,12 +14,12 @@ void	recv_msg(t_ping *ping, t_ping_pkt *pckt)
 		ft_printf("From %s icmp_seq=%d Destination Host Unreachable\n", recv_ip, ping->msg_count);
 	else
 	{
-    	if ((ping->aft.tv_usec -= ping->def.tv_usec) < 0)
+    	if ((ping->aft.tv_usec -= ping->bef.tv_usec) < 0)
     	{
     	    --ping->aft.tv_sec;
     	    ping->aft.tv_usec += 1000000;
     	}
-    	ping->aft.tv_sec -= ping->def.tv_sec;
+    	ping->aft.tv_sec -= ping->bef.tv_sec;
 		rtt = ping->aft.tv_sec * 1000.0 + ping->aft.tv_usec / 1000.0;
 		rtt = rtt - (ping->bef.tv_sec * 1000.0 + ping->bef.tv_usec / 1000.0);
 		ft_printf("%d bytes from %s (%s): icmp_seq=%d ttl=%d time=%.2f ms\n",  

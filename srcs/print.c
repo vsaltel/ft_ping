64080418f6863521e1c01a7209091083d66bb686
Valentin <saltel.valentin@gmail.com>
2021-04-06 14:15:44 +0200

    test1

--- srcs/print.c
+++ srcs/print.c
@@ -6,7 +6,7 @@ void	print_final_stats(t_ping *ping)
 	double	rtt;
 
 	gettimeofday(&end_time, NULL);
-   	tv_substract(&end_time, &ping->launch_time);	 
+   	tv_subtract(&end_time, &ping->launch_time);	 
 	rtt = end_time.tv_sec * 1000.0 + end_time.tv_usec / 1000.0;
 	rtt = rtt - (ping->launch_time.tv_sec * 1000.0 + ping->launch_time.tv_usec / 1000.0);
 	ft_printf("--- %s ping statistics ---\n", ping->dest_name);

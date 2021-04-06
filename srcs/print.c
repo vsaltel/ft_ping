5b0861d46a65ef25af622513d3c3de19dd63aca3
Valentin <saltel.valentin@gmail.com>
2021-04-06 18:11:32 +0200

    rtt

--- srcs/print.c
+++ srcs/print.c
@@ -13,7 +13,7 @@ void	print_final_stats(t_ping *ping)
 	total_time = end_time.tv_sec * 1000 + end_time.tv_usec / 1000;
 	total_time = total_time - (ping->launch_time.tv_sec * 1000 + ping->launch_time.tv_usec / 1000);
 	mean = ping->rtt_sum / ping->msg_recv_count;
-	smean = ping->rtt_sum_sq / ping->ping->msg_recv_count;
+	smean = ping->rtt_sum_sq / ping->msg_recv_count;
 	rtt_mdev = sqrt(smean - (mean * mean));
 	ft_printf("--- %s ping statistics ---\n", ping->dest_name);
 	ft_printf("%d packets transmitted, %d received, %d%% packet loss, time: %ld ms\n",

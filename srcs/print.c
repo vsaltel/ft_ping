e68f7355eaa6e05a79a7b0a194e5ff4ed28e9718
Valentin <saltel.valentin@gmail.com>
2021-04-06 16:13:24 +0200

    working (without -v)

--- srcs/print.c
+++ srcs/print.c
@@ -7,9 +7,7 @@ void	print_final_stats(t_ping *ping)
 
 	gettimeofday(&end_time, NULL);
    	//tv_subtract(&end_time, &ping->launch_time);	 
-	ft_printf("b s %d u %d\na s %d u %d\n", ping->launch_time.tv_sec, ping->launch_time.tv_usec, end_time.tv_sec, end_time.tv_usec);
 	rtt = end_time.tv_sec * 1000 + end_time.tv_usec / 1000;
-	ft_printf("rtt bef : %ld - %ld\n", rtt, (ping->launch_time.tv_sec * 1000 + ping->launch_time.tv_usec / 1000));
 	rtt = rtt - (ping->launch_time.tv_sec * 1000 + ping->launch_time.tv_usec / 1000);
 	ft_printf("--- %s ping statistics ---\n", ping->dest_name);
 	ft_printf("%d packets transmitted, %d received, %d%% packet loss, time: %ld ms\n",

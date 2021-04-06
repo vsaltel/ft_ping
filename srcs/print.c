fb94499a47abc7cc3bc8752942b9d6ce4c52e99d
Valentin <saltel.valentin@gmail.com>
2021-04-06 16:24:41 +0200

    working (without -v)

--- srcs/print.c
+++ srcs/print.c
@@ -6,7 +6,6 @@ void	print_final_stats(t_ping *ping)
 	long	total_time;
 
 	gettimeofday(&end_time, NULL);
-   	tv_subtract(&end_time, &ping->launch_time);	 
 	total_time = end_time.tv_sec * 1000 + end_time.tv_usec / 1000;
 	total_time = total_time - (ping->launch_time.tv_sec * 1000 + ping->launch_time.tv_usec / 1000);
 	ft_printf("--- %s ping statistics ---\n", ping->dest_name);

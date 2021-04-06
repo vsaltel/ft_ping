527e81b316e097ead25a1c3748c7cfa6c76d4af1
Valentin <saltel.valentin@gmail.com>
2021-04-06 15:41:01 +0200

    test1

--- srcs/print.c
+++ srcs/print.c
@@ -3,14 +3,14 @@
 void	print_final_stats(t_ping *ping)
 {
 	struct timeval end_time;
-	double	rtt;
+	long	rtt;
 
 	gettimeofday(&end_time, NULL);
    	//tv_subtract(&end_time, &ping->launch_time);	 
 	rtt = end_time.tv_sec * 1000.0 + end_time.tv_usec / 1000.0;
 	rtt = rtt - (ping->launch_time.tv_sec * 1000.0 + ping->launch_time.tv_usec / 1000.0);
 	ft_printf("--- %s ping statistics ---\n", ping->dest_name);
-	ft_printf("%d packets transmitted, %d received, %d%% packet loss, time: %d ms\n",
+	ft_printf("%d packets transmitted, %d received, %d%% packet loss, time: %ld ms\n",
 		ping->msg_count, ping->msg_recv_count,
 		((ping->msg_count - ping->msg_recv_count)/ping->msg_count) * 100, rtt/* ping->total_stime / 1000*/);
 }

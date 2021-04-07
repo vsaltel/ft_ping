96589057974bd6a166f36556a96ffa51c39f1b4e
Valentin <saltel.valentin@gmail.com>
2021-04-07 14:33:45 +0200

    ttl

--- srcs/send_msg.c
+++ srcs/send_msg.c
@@ -6,22 +6,6 @@ void	send_msg(void)
 	int			ret;
 	struct icmp	*icmp;
 
-/*
-	ip = (struct ip *)g_ping.sendbuf;
-	ip->ip_hl = 5;
-	ip->ip_v = 4;
-	ip->ip_tos = 0;
-	ip->ip_len = sizeof(struct ip) + sizeof(struct icmp);
-	ip->ip_id = g_ping.pid;
-	ip->ip_off = 0;
-	ip->ip_ttl = g_ping.ttl;
-	ip->ip_p = IPPROTO_ICMP;
-	len = 8 + g_ping.datalen;
-	ip->ip_sum = checksum((u_short *) ip, len);
-	struct sockaddr_in *tmp;
-	tmp = (struct sockaddr_in *)g_ping.pr.sasend;
-	ip->ip_dst = tmp->sin_addr; 
-*/
 	icmp = (struct icmp *)g_ping.sendbuf;
 	icmp->icmp_type = ICMP_ECHO;
 	icmp->icmp_code = 0;
@@ -29,15 +13,12 @@ void	send_msg(void)
 	icmp->icmp_seq = g_ping.msg_sent;
 	ft_memset(icmp->icmp_data, 0xa5, g_ping.datalen);
 	gettimeofday(&g_ping.bef, NULL);
-	icmp->icmp_data = malloc(50);
-	//gettimeofday((struct timeval *)icmp->icmp_data, NULL);
-		struct timeval *tmp;
-		tmp = (struct timeval *)icmp->icmp_data;
-		ft_printf("send len %ld usec %d\n", sizeof(struct timeval), tmp->tv_usec);
-
+	//icmp->icmp_data = malloc(50);
+	gettimeofday((struct timeval *)icmp->icmp_data, NULL);
 	len = 8 + g_ping.datalen;
 	icmp->icmp_cksum = 0;
 	icmp->icmp_cksum = checksum((u_short *) icmp, len);
+		ft_printf("send len %ld %d\n", sizeof(struct timeval), len);
 	ret = sendto(g_ping.sockfd, g_ping.sendbuf, len, 0, g_ping.pr.sasend, g_ping.pr.salen);
 	if (ret)
 		g_ping.msg_sent++;

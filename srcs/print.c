88ee90a9c26f6bfac1e1ea32f884bea3c774ffea
Valentin <saltel.valentin@gmail.com>
2021-04-07 16:34:50 +0200

    -q

--- srcs/print.c
+++ srcs/print.c
@@ -34,6 +34,8 @@ void	print_final_stats(t_ping *ping)
 	gettimeofday(&end_time, NULL);
 	total_time = end_time.tv_sec * 1000 + end_time.tv_usec / 1000;
 	total_time = total_time - (ping->launch_time.tv_sec * 1000 + ping->launch_time.tv_usec / 1000);
+	if (ping->q)
+		ft_printf("\n");
 	ft_printf("--- %s ping statistics ---\n", ping->dest_name);
 	ft_printf("%d packets transmitted, %d received, %d%% packet loss, time: %ld ms\n",
 		ping->msg_count, ping->msg_recv_count,

01a3396016c0a1f195363f40aeb4090715036927
Valentin <saltel.valentin@gmail.com>
2021-04-08 16:09:10 +0200

    -t maj

--- srcs/ping.c
+++ srcs/ping.c
@@ -32,6 +32,15 @@ int	ping(t_ping *ping)
 	if (!(res = reverse_dns_info(ping->dest_name, NULL, AF_INET, 0)))
 		return (-2);
 	ping->dest_ip = set_inetaddr(res->ai_addr);
+
+	char buf[BUFSIZE];
+	len = sizeof(struct sockaddr_in);
+    if (getnameinfo(&res->ai_addr, len, buf, sizeof(buf), NULL, 0, NI_NAMEREQD))
+        printf("could not resolve hostname\n");
+    else
+		printf("host=%s\n", buf);
+      
+
 	ft_printf("FT_PING %s (%s) %d(%d) data bytes\n",
 		res->ai_canonname ? res->ai_canonname : ping->dest_name,
 		ping->dest_ip, ping->datalen, ping->datalen + sizeof(struct iphdr) + sizeof(struct icmphdr));

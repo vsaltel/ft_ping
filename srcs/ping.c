1a25840b4b9d1fd8fdca2a43686d6ec8c0bff7dd
Valentin <saltel.valentin@gmail.com>
2021-04-08 16:10:08 +0200

    -t maj

--- srcs/ping.c
+++ srcs/ping.c
@@ -34,7 +34,7 @@ int	ping(t_ping *ping)
 	ping->dest_ip = set_inetaddr(res->ai_addr);
 
 	char buf[BUFSIZE];
-	len = sizeof(struct sockaddr_in);
+	socklen_t len = sizeof(struct sockaddr_in);
     if (getnameinfo(res->ai_addr, len, buf, sizeof(buf), NULL, 0, NI_NAMEREQD))
         printf("could not resolve hostname\n");
     else

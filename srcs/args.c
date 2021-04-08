eedfb47bd0eb9efa636e36a5600c0a7e97430d2d
Valentin <saltel.valentin@gmail.com>
2021-04-08 22:43:03 +0200

    norme

--- srcs/args.c
+++ srcs/args.c
@@ -13,46 +13,65 @@ int	check_options(t_ping *ping)
 		return (1);
 	if (ping->ttl <= 0 || ping->ttl > 255)
 	{
-		ft_printf("ft_ping: can't set unicast time-to-live: Invalid argument\n");
+		ft_dprintf(2, "ft_ping: can't set unicast time-to-live: Invalid argument\n");
 		return (1);
 	}
 	if (ping->datalen < 0 || ping->datalen > BUFSIZE)
 	{
-		ft_printf("ft_ping: illegal packet size\n");
+		ft_dprintf(2, "ft_ping: illegal packet size\n");
 		return (1);
 	}
 	else if (ping->count_max <= 0 && ping->count_max != -1)
 	{
-		ft_printf("ft_ping: bad number of packets to transmit.\n");
+		ft_dprintf(2, "ft_ping: bad number of packets to transmit.\n");
 		return (1);
 	}
 	return (0);
 }
-int	set_option(t_ping *ping, int ac, char **av, int *n)
+
+int	set_option(t_ping *ping, char **av, int *n, int x)
 {
-	if ((av[*n][1] == 't' || av[*n][1] == 's' || av[*n][1] == 'c') && *n + 1 == ac)
-	{
-		ft_printf("ft_ping: option requires an argument -- '%c'\n", av[*n][1]);
-		return (1);
-	}
-	if (av[*n][1] == 't')
+	if (av[*n][x] == 't')
 		ping->ttl = ft_atoi(av[++(*n)]);
-	else if (av[*n][1] == 's')
+	else if (av[*n][x] == 's')
 		ping->datalen = ft_atoi(av[++(*n)]);
-	else if (av[*n][1] == 'c')
+	else if (av[*n][x] == 'c')
 		ping->count_max = ft_atoi(av[++(*n)]);
-	else if (av[*n][1] == 'h')
+	else if (av[*n][x] == 'h')
 		ping->h = 1;
-	else if (av[*n][1] == 'v')
+	else if (av[*n][x] == 'v')
 		ping->v = 1;
-	else if (av[*n][1] == 'q')
+	else if (av[*n][x] == 'q')
 		ping->q = 1;
-	else if (av[*n][1] == 'D')
+	else if (av[*n][x] == 'D')
 		ping->d = 1;
 	else
 	{
-		ft_printf("ft_ping: invalid option -- '%c'\n", av[*n][1]);
+		ft_dprintf(2, "ft_ping: invalid option -- '%c'\n", av[*n][1]);
 		return (1);
+	}
+	return (0);
+}
+
+int	set_option_loop(t_ping *ping, int ac, char **av, int *n)
+{
+	int	x;
+
+	x = 0;
+	while (av[*n][++x])
+	{
+		if (av[*n][1] == 't' || av[*n][1] == 's' || av[*n][1] == 'c')
+		{
+			if (av[*n][x + 1])
+				return (1);
+ 			if (*n + 1 == ac)
+			{
+				ft_dprintf(2, "ft_ping: option requires an argument -- '%c'\n", av[*n][1]);
+				return (1);
+			}
+		}
+		if (set_option(ping, av, n, x))
+			return (1);
 	}
 	return (0);
 }
@@ -66,7 +85,7 @@ int	get_args(t_ping *ping, int ac, char **av)
 	{
 		if (av[n][0] && av[n][0] == '-')
 		{
-			if (set_option(ping, ac, av, &n))
+			if (set_option_loop(ping, ac, av, &n))
 				return (1);
 		}
 		else if (ping->dest_name)
@@ -78,117 +97,7 @@ int	get_args(t_ping *ping, int ac, char **av)
 		return (1);
 	return (0);
 }
-/*
-void	get_args(t_ping *ping, int ac, char **av)
-{
-	int		n;
-	int		x;
 
-	n = 0;
-	while (++n < ac)
-	{
-		if (av[n][0] && av[n][0] == '-')
-		{
-			x = 1;
-			if (av[n][x] == 't')
-			{
-				ping->ttl = ft_atoi(av[++n]);
-				continue ;
-			}
-			if (av[n][x] == 's')
-			{
-				ping->datalen = ft_atoi(av[++n]);
-				continue ;
-			}
-			if (av[n][x] == 'c')
-			{
-				ping->count_max = ft_atoi(av[++n]);
-				continue ;
-			}
-			if (av[n][x] == 'h')
-				ping->h = 1;
-			else if (av[n][x] == 'v')
-				ping->v = 1;
-			else if (av[n][x] == 'q')
-				ping->q = 1;
-			else if (av[n][x] == 'D')
-				ping->d = 1;
-		}
-		else
-			ping->dest_name = strdup(av[n]);
-	}
-}
-
-int	check_args(int ac, char **av)
-{
-	int		n;
-	int		x;
-	int		dest;
-
-	dest = 0;
-	n = 0;
-	while (++n < ac)
-	{
-		if (av[n][0] && av[n][0] == '-')
-		{
-			x = 1;
-			if (av[n][1] == 's' && av[n][2] == '\0')
-			{
-				if (++n == ac)
-				{
-					ft_printf("ft_ping: option requires an argument -- 's'\n");
-					return (1);
-				}
-				else if (ft_atoi(av[n]) < 0 || ft_atoi(av[n]) > BUFSIZE)
-				{
-					ft_printf("ft_ping: illegal packet size\n");
-					return (1);
-				}
-				continue ;
-			}
-			if (av[n][1] == 't' && av[n][2] == '\0')
-			{
-				if (++n == ac)
-				{
-					ft_printf("ft_ping: option requires an argument -- 't'\n");
-					return (1);
-				}
-				else if (ft_atoi(av[n]) <= 0 || ft_atoi(av[n]) > 255)
-				{
-					ft_printf("ft_ping: can't set unicast time-to-live: Invalid argument\n");
-					return (1);
-				}
-				continue ;
-			}
-			if (av[n][1] == 'c' && av[n][2] == '\0')
-			{
-				if (++n == ac)
-				{
-					ft_printf("ft_ping: option requires an argument -- 'c'\n");
-					return (1);
-				}
-				else if (ft_atoi(av[n]) <= 0)
-				{
-					ft_printf("ft_ping: bad number of packets to transmit.\n");
-					return (1);
-				}
-				continue ;
-			}
-			while (av[n][x])
-			{
-				if (!strchr(OPTIONS, av[n][x]))
-					return (1);
-				x++;
-			}
-		}
-		else if (++dest > 1)
-			return (1);
-	}
-	if (!dest)
-		return (1);
-	return (0);
-}
-*/
 void	init_ping(t_ping *ping)
 {
 	ping->v = 0;

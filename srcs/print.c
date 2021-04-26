1da74386a7a9404b718c99f956fb4feb56d55e03
Valentin <saltel.valentin@gmail.com>
2021-04-26 15:53:31 +0200

    time

--- srcs/print.c
+++ srcs/print.c
@@ -49,9 +49,8 @@ void	print_final_stats(t_ping *ping)
 		ft_printf("\n");
 	ft_printf("--- %s ping statistics ---\n", ping->dest_name);
 	if (!ping->msg_count)
-		percent = 0;
-	else
-		percent = ((ping->msg_count - ping->msg_recv_count) / ping->msg_count) * 100;
+		ping->msg_count++;
+	percent = ((ping->msg_count - ping->msg_recv_count) / ping->msg_count) * 100;
 	ft_printf("%d packets transmitted, %d received, "
 		"\%d%% packet loss, time: %ld ms\n",
 		ping->msg_count, ping->msg_recv_count, percent, total_time);

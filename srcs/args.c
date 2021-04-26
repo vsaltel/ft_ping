bfc7a328d5ee8192ef8c3f282204e3bb1ff96050
Valentin <saltel.valentin@gmail.com>
2021-04-26 16:12:18 +0200

    time

--- srcs/args.c
+++ srcs/args.c
@@ -3,7 +3,7 @@
 static int	check_options(t_ping *ping)
 {
 	if (ping->datalen > BUFSIZE)
-		g_ping.datalen = BUFSIZE - 8;
+		g_ping.datalen = BUFSIZE - sizeof(struct icmp);
 	if (!ping->dest_name)
 		return (1);
 	if (ping->ttl <= 0 || ping->ttl > 255)
